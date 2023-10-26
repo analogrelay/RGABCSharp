959f99f18f582461cece4d84ecbc83cb20355a8d
Ashley Stanton-Nurse <github@analogrelay.net>
2023-10-26 12:13:12 -0700

    Changes made from the Regression Games Agent Builder

--- BotEntryPoint.cs
+++ BotEntryPoint.cs
@@ -12,16 +12,23 @@ namespace RgAbilitybotCs
 
         protected override void ConfigureBotInternal(RG rgObject)
         {
-            rgObject.CharacterConfig = "";
+            rgObject.CharacterConfig = new Dictionary<string, object>()
+            {
+            };
         }
 
         protected override RootNode BuildBehaviorTree()
         {
             var topLevelSequenceNode = new SequenceNode("Top Level Sequence Node");
             topLevelSequenceNode.AddChild(new IsBotActive());
+
             topLevelSequenceNode.AddChild(new PerformAction());
+
             var rootNode = new RootNode("Root Node");
             rootNode.AddChild(topLevelSequenceNode);
+
+
+            return rootNode;
         }
     }
 }
---
+++ NodesPerformAction.cs
@@ -0,0 +1,17 @@
+namespace RgAbilitybotCs
+{
+    public class PerformAction : ActionNode
+    {
+        public PerformAction() : base("Perform Action")
+        {
+        }
+
+        /**
+         * Generated from prompt:
+         * 
+         */
+        public override NodeStatus Execute(RG rgObject)
+        {
+        }
+    }
+}
