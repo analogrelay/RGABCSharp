b9ae34a4306da70b61df9b530107c019d8d9f071
Ashley Stanton-Nurse <github@analogrelay.net>
2023-10-26 11:53:47 -0700

    Changes made from the Regression Games Agent Builder

--- BotEntryPoint.cs
+++ BotEntryPoint.cs
@@ -1,1 +1,27 @@
-// SEE SHARP!
+using System.Collections.Generic;
+using RegressionGames.RGBotLocalRuntime;
+using RegressionGames.StateActionTypes;
+using UnityEngine;
+
+namespace RgAbilitybotCs
+{
+    public class BotEntryPoint : RGBehaviorTreeBot
+    {
+        protected override bool GetIsSpawnable() => true;
+        protected override RGBotLifecycle GetBotLifecycle() => RGBotLifecycle.MANAGED;
+
+        protected override void ConfigureBotInternal(RG rgObject)
+        {
+            rgObject.CharacterConfig = "";
+        }
+
+        protected override RootNode BuildBehaviorTree()
+        {
+            var topLevelSequenceNode = new SequenceNode("Top Level Sequence Node");
+            topLevelSequenceNode.AddChild(new IsBotActive());
+            topLevelSequenceNode.AddChild(new PerformAction());
+            var rootNode = new RootNode("Root Node");
+            rootNode.AddChild(topLevelSequenceNode);
+        }
+    }
+}
