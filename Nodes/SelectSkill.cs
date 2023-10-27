73ac96c574e18e52223230eff13c8ff1fee487f4
Ashley Stanton-Nurse <github@analogrelay.net>
2023-10-27 09:41:53 -0700

    Changes made from the Regression Games Agent Builder

---
+++ Nodes/SelectSkill.cs
@@ -0,0 +1,26 @@
+using RegressionGames.BehaviorTree;
+using RegressionGames.RGBotLocalRuntime;
+using RegressionGames.StateActionTypes;
+using UnityEngine;
+
+namespace RgAbilitybotCs
+{
+    public class SelectSkill : ActionNode
+    {
+        public SelectSkill() : base("Select Skill")
+        {
+        }
+
+        /**
+         * Generated from prompt:
+         * 
+         */
+        protected override NodeStatus Execute(RG rgObject)
+        {
+        	var target = GetData<RGStateEntity>("targetEntity");
+        	var selectedSkill = target.GetValueOrDefault("team", 0) is 1 ? 1 : 0;
+        	SetData("selectedSkill", selectedSkill);
+        	return NodeStatus.Success;
+        }
+    }
+}
